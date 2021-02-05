5057996a8070e60ca97dafb0e033f1c743a60a53
Kunal Verma <55757204+kunal-ai@users.noreply.github.com>
2021-02-05 14:22:16 -0800

    File Upload

---
+++ index1.js
@@ -0,0 +1,108 @@
+
+$(document).ready(function(){
+  $("#td1").click(function(){
+    $("#td1").hide();
+  })
+})
+
+$(document).ready(function(){
+  $("#td2").click(function(){
+    $(this).hide();
+  });
+});
+
+$(document).ready(function(){
+  $("#td3").click(function(){
+    $(this).hide();
+  });
+});
+
+$(document).ready(function(){
+  $("#td4").click(function(){
+    $("#td4").toggle("scale");
+  })
+})
+
+$(document).ready(function(){
+  $("#td5").click(function(){
+    $("#td5").toggle("puff");
+  })
+})
+
+$(document).ready(function(){
+  $("#td6").click(function(){
+    $("#td6").toggle("explode");
+  });
+});
+
+$(document).ready(function(){
+  $("#td7").click(function(){
+    $(this).hide();
+  })
+})
+
+$(document).ready(function(){
+  $("#td8").click(function(){
+    $(this).hide();
+  });
+});
+
+$(document).ready(function(){
+  $("#td9").click(function(){
+    $(this).hide();
+  });
+});
+
+$(document).ready(function(){
+  $("#td10").click(function(){
+    $("#td10").toggle("scale");
+  })
+})
+
+$(document).ready(function(){
+  $("#td11").click(function(){
+    $("#td11").toggle("puff");
+  })
+})
+
+$(document).ready(function(){
+  $("#td12").click(function(){
+    $("#td12").toggle("explode");
+  });
+});
+
+$(document).ready(function(){
+  $("#td13").click(function(){
+    $(this).hide();
+  })
+})
+
+$(document).ready(function(){
+  $("#td14").click(function(){
+    $(this).hide();
+  });
+});
+
+$(document).ready(function(){
+  $("#td15").click(function(){
+    $(this).hide();
+  });
+});
+
+$(document).ready(function(){
+  $("#td16").click(function(){
+    $("#td16").toggle("scale");
+  })
+})
+
+$(document).ready(function(){
+  $("#td17").click(function(){
+    $("#td17").toggle("puff");
+  })
+})
+
+$(document).ready(function(){
+  $("#td18").click(function(){
+    $("#td18").toggle("explode");
+  });
+});
